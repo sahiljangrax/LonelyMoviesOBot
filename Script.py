937bcb80e299a3096c0ba6e91e4942f06520f7b0
sahiljangrax <157376542+sahiljangrax@users.noreply.github.com>
2025-01-31 23:14:23 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -119,7 +119,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b><code>{file_name}</code>\n<blockquote>फिल्म चलेगी नहीं अगर अपने नीचे दिए गए ग्रुप्स जॉइन नहीं किए👇</blockquote>\n<blockquote>Movie Is Not Watchable Without Joining These Given Groups👇</blockquote>\n<blockquote>Film Chalegi Nahi Chalegi Agar Aap Niche Diye Gaye Groups ko Join Nahi kiya👇</blockquote><blockquote>Movie Request Groups✅ -> 
+    FILE_CAPTION = """<b><code>{file_name}</code>\n<blockquote>फिल्म चलेगी नहीं अगर अपने नीचे दिए गए ग्रुप्स जॉइन नहीं किए👇</blockquote>\n<blockquote>Movie Is Not Watchable Without Joining These Given Groups👇</blockquote>\n<blockquote>Film Chalegi Nahi Chalegi Agar Aap Niche Diye Gaye Groups ko Join Nahi kiya👇\n</blockquote><blockquote>Movie Request Groups✅ -> 
 1. https://t.me/+Ns2TuI5KAKVmYjll
 2. https://t.me/MovieFinderGroupByLM
 3. https://t.me/MovieSearchGroupByLM</blockquote>\n 𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/Lonely_Movies">Lonely Movies</a>」
